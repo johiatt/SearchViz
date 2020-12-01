464f8133640ff462f2f45b019d8b1537bb9e42ec
jordan h <jordanhiatt@u.boisestate.edu>
2020-12-01 15:10:43 -0700

    checkerboard works

--- playgroundTest/src/playgroundTest/GUI.java
+++ playgroundTest/src/playgroundTest/GUI.java
@@ -2,18 +2,21 @@ package playgroundTest;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class GUI extends JFrame {
 	
+	public Board board;
 	int width = 1024;
 	int height = 1024;
-	int squares = 20;
+	int squares = 5;
 	int squareSide = (int) (width/squares);
 	boolean alternate = true;
-
 	
 	public GUI() {
 		this.setTitle("SearchViz");
@@ -21,8 +24,22 @@ public class GUI extends JFrame {
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setVisible(true);
 		this.setResizable(false);
-		Board board = new Board();
+		
+		board = new Board();
 		this.setContentPane(board);
+		//button
+		JButton b=new JButton("Click Here");  
+	    b.setBounds(50,100,95,30);  
+	    this.add(b);   
+	    b.addActionListener(new ActionListener(){  
+	    	public void actionPerformed(ActionEvent e){  
+	    		alternate = !alternate;
+	    		board.revalidate();
+	    		board.repaint();
+	    		
+	        }  
+	    });   
+		
 	}
 	
 	public class Board extends JPanel {
@@ -31,10 +48,18 @@ public class GUI extends JFrame {
 			g.fillRect(0, 0, width, height);
 			for(int i = 0; i < squares; i++) {
 				for(int j = 0; j < squares; j++) {
-					if(i % 2 == j % 2) {
-						g.setColor(Color.GRAY);
+					if(alternate) {
+						if((i % 2 == j % 2) ) {
+							g.setColor(Color.GRAY);
+						} else {
+							g.setColor(Color.BLUE);
+						}
 					} else {
-						g.setColor(Color.BLUE);
+						if((i % 2 == j % 2) ) {
+							g.setColor(Color.BLUE);
+						} else {
+							g.setColor(Color.GRAY);
+						}
 					}
 					g.fillRect((int)j*(squareSide), (int)i*(squareSide), squareSide, squareSide);
 				}
--- playgroundTest/src/playgroundTest/Main.java
+++ playgroundTest/src/playgroundTest/Main.java
@@ -7,7 +7,7 @@ public class Main {
 	       SwingUtilities.invokeLater(new Runnable() {
 	   		public void run() {
 	   		    new GUI();
-	   		}
+	   			}
 	   	    });
 	}
 }
